73b66545f18f4244de956ab107518c7e1a1e26ea
ashabib1 <ashabib1@g.ucla.edu>
2023-04-28 07:31:00 -0700

    second attempt f1 score: 0.7

--- basic_alinger.py
+++ basic_alinger.py
@@ -1,15 +1,10 @@
-
 def parse_reads_file(reads_fn):
 
     try:
         with open(reads_fn, 'r') as rFile:
-            print("Parsing...")
-            count = 0
+            print("Parsing Reads...")
             all_reads = []
             for line in rFile:
-                count += 1
-                if count % 1000 == 0:
-                    print(count, " reads done")
                 ends = line.strip().split(',')
                 all_reads.append(ends)
         return all_reads
@@ -21,7 +16,7 @@ def parse_ref_file(ref_fn):
 
     try:
         with open(ref_fn, 'r') as gFile:
-            print("Parsing...")
+            print("Parsing Reference...")
             first_line = True
             ref_genome = ''
             for line in gFile:
@@ -62,42 +57,74 @@ def find_error_index(a, b):
         i += 1
     return indices
 
+def remove(string, i):
+
+    a = string[:i]
+    b = string[i+1:]
+    return a + b
+
+
+
     
 if __name__ ==  "__main__":
 
-    THRESHOLD = 0.97
+    THRESHOLD = 0.95
+    DELETION_THRESHOLD = 0.6
 
     reads = parse_reads_file('project1a_10000_with_error_paired_reads.fasta')
     reference = parse_ref_file('project1a_10000_reference_genome.fasta')
     max_match_list = []
     outputs = []
+    initial = 0
+    insertion = False
 
     for j in range(len(reads)):
         if reads[j][0][0] == '>':
             continue
+        if j%4==3 and insertion==True:
+            initial = saved_index
+        else:
+            initial = 0
+        insertion = False
         current_read = reads[j][0]
         match_list = []
-        for i in range(0, len(reference)-len(current_read)):
+        for i in range(initial, len(reference)-len(current_read)):
             score = comparison(current_read, reference[i:i+len(current_read)])
             match_list.append(score)
             if score / len(current_read) > THRESHOLD:
                 err_index = find_error_index(current_read, reference[i:i+len(current_read)])
                 try:
-                    if err_index[0] > len(current_read) - 5:
+                    if err_index[0] > len(current_read) - 5 or err_index[len(err_index) - 1] < 5:
                         continue
                 except:
-                    pass
-                try:
-                    if err_index[len(err_index) - 1] < 5:
-                        continue
-                except:
-                    pass
+                     pass
+                saved_index = i + len(current_read)
+                insertion=True
                 for index in err_index:
                     potential_output = '>S' + str(match_list.index(max(match_list)) + index) + ' ' + str(reference[i + index]) + ' ' + str (current_read[index])
-                    if potential_output not in outputs:
-                        outputs.append(potential_output)
-                    #print(potential_output)
+                    #if potential_output not in outputs:
+                    outputs.append(potential_output)
+            # elif score / len(current_read) > DELETION_THRESHOLD:
+            #     current_reference = reference[i:i+len(current_read)]
+            #     for j in range(len(current_reference)):
+            #         new_reference = remove(current_reference, j)
+            #         new_score = comparison(current_read[:-1], new_reference)
+            #         if new_score / (len(current_read) - 1) > THRESHOLD:
+            #             # err_index = find_error_index(current_read, new_reference[j:j+len(current_read)])
+            #             # for index in err_index:
+            #             #     potential_output = '>D' + str(match_list.index(max(match_list)) + j) + ' ' + str(new_reference[j + index])
+            #             #     if potential_output not in outputs:
+            #             #         outputs.append(potential_output)
+            #             if j < 5 or j > 45:
+            #                 continue
+            #             potential_output = '>D' + str(match_list.index(max(match_list)) + j - 1) + ' ' + str(reference[i + j])
+            #             if potential_output not in outputs:
+            #                 outputs.append(potential_output)
         max_match_list.append(max(match_list))
     outputs.sort(key=lambda x:int(x.split(' ')[0][2:]))
+    final_outputs = []
+    for output in outputs:
+        if outputs.count(output) > 1 and output not in final_outputs:
+            final_outputs.append(output)
     
-    open_file('readme2.txt', outputs)
+    open_file('readme2.txt', final_outputs)
